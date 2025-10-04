20abed7ce9b1b902b20f69f72c2c075a954b8d19
SirIgnaz <15999046+SirIgnaz@users.noreply.github.com>
2025-10-04 15:34:41 +0200

    Add configurable face detector size

--- modules/globals.py
+++ modules/globals.py
@@ -43,6 +43,7 @@ mask_down_size = 0.50
 mask_size = 1
 codeformer_mask_blur = 45
 codeformer_color_strength = 1.0
+face_detector_size = (640, 640)
 
 # DirectML face enhancement can be enabled via UI or environment variable. Keep
 # the environment variable compatibility so headless executions behave the same
